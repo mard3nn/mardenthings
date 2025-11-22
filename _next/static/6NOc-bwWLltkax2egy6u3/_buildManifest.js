09ed80e8f9b487499fe38aa0647cf39e2e0dc825
mard3nn <mard3nn@gmail.com.com>
2025-11-22 20:34:22 +0500

    Updates

--- _next/static/6NOc-bwWLltkax2egy6u3/_buildManifest.js
+++ _next/static/6NOc-bwWLltkax2egy6u3/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/mardenthings//_next/:path+",
-        "destination": "/mardenthings/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
